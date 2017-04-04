325e856158df7194f7682d54537d339773ec87c1
DUSHYANT SINGH <dushyant7917@gmail.com>
2017-04-04 20:58:24 +0530

    clicking on all select values

--- feedback.py
+++ feedback.py
@@ -14,7 +14,7 @@ time.sleep(4)
 search = driver.find_element_by_id('Email')
 search.send_keys('dushyantsingh_na@srmuniv.edu.in')
 search = driver.find_element_by_id('Password')
-search.send_keys('password')
+search.send_keys('pass')
 search = driver.find_element_by_id('signinBtn').click()
 
 print "waiting for 4 secs for giving browser time to load the homepage..."
@@ -33,17 +33,17 @@ while tries < 10:
         time.sleep(4)
         tries += 1
 
-print "waiting for 4 secs for giving browser time to load the feedback page..."
-time.sleep(4)
+print "waiting for 10 secs for giving browser time to load the feedback page..."
+time.sleep(10)
 
 tries = 0
 while tries < 10:
 
     try:
-        driver.execute_script("window.scrollBy(500, 0);")
-        driver.execute_script("window.scrollBy(0, 200);")
-        search = driver.find_element_by_xpath("//td[@class='zc-button-row']/span/input[@value='Submit']")
-        print "Submit button found!"
+        search = driver.find_element_by_xpath("//td[@class='resetGridClass']").click()
+        for i in search:
+            if i.text == "Average":
+                i.click()
         break
 
     except Exception as e:
@@ -51,3 +51,7 @@ while tries < 10:
         print "waiting for 4 secs before trying again..."
         time.sleep(4)
         tries += 1
+
+
+search = driver.find_element_by_xpath("//td[@class='zc-button-row']/span/input[@value='Submit']").click()
+print "Submit button clicked!"
